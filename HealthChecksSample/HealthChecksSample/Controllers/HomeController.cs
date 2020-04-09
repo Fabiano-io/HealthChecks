79025c73cf08ada6f9b281acd4397a0a250ec460
Fabiano de Moraes Rodrigues <ti.fabianorodrigues@conectcar.com>
2020-04-09 16:51:33 -0300

    Inclusão do Projeto

---
+++ HealthChecksSample/HealthChecksSample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthChecksSample.Controllers
+{
+    [Route("[controller]")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet]
+        public void index()
+        {
+
+        }
+    }
+}
